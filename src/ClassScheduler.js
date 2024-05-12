d6321a4736d938fd0c07bed577c5ad4ed64fe98d
BOT1LEO <66963910+BOT1LEO@users.noreply.github.com>
2024-05-12 23:49:44 +0100

    Aumento de 10x na preformance facil

--- src/ClassScheduler.js
+++ src/ClassScheduler.js
@@ -4,9 +4,6 @@ const util = require('util');
 const { get } = require("node:http");
 const { time } = require("node:console");
 
-var classroom = null;
-var horario = null;
-
 
 
 fs.readFile("CaracterizaçãoDasSalas.csv", 'utf8', (err, data) => {
@@ -14,97 +11,65 @@ fs.readFile("CaracterizaçãoDasSalas.csv", 'utf8', (err, data) => {
         console.error(err);
         return;
     }
-    classroom = data;
-    horarioFile();
+    var classroom = data;
+    horarioFile(classroom);
 });
-function horarioFile(){
+function horarioFile(classroom){
     fs.readFile("HorarioDeExemplo.csv", 'utf8', (err, data) => {
         if (err) {
             console.error(err);
             return;
         }
-        horario = data;
-        getSalas();
+        var horario = data;
+        listSalass(classroom,horario);
     });
 }
 
 
-function getSalas(){
+function listSalass(classroom,horario){
     Papa.parse(classroom, {
         header:true,
         complete: function(results) {
             classroom = results.data;
-            getHorario();
+            getHorario(classroom,horario);
         }
     });
 }
 
-function getHorario(){
+function getHorario(classroom,horario){
     Papa.parse(horario, {
         header:true,
         complete: function(results) {
             horario = results.data;
-            //getSala();
-            //getDisponibilidadeSala('C7.09',new Date(2022,10,30));
             var startTime = performance.now()
 
-            findOpenSlots(new Date(2022,10,21),new Date(2022,10,27)); 
+            findOpenSlots(classroom,horario,new Date(2022,10,21),new Date(2022,10,27)); 
                 
             var endTime = performance.now()
 
             console.log(`Call to doSomething took ${endTime - startTime} milliseconds`)
-            
-            //findClassTimes();
         }
     });
 }
 
-function findClassTimes(){
-    /*console.log( horario.reduce( (acc, curr) => {
-        if (!acc.includes(curr['Hora início da aula']+"-"+curr['Hora fim da aula']))
-            acc.push(curr['Hora início da aula']+"-"+curr['Hora fim da aula']);
-        return acc;
-    }, []).sort((a,b) => {
-        if(a != undefined && b != undefined){
-            const t1 = a.split(":");
-            const t2 = b.split(":");
-            if(t1[0]!= t2[0]){
-                return parseInt(t1[0])-parseInt(t2[0]);
-            } else {
-                return parseInt(t1[1])-parseInt(t2[1]);
-            } 
-        }
-        return 0;
-    }));*/
-    console.log(horario.reduce( (acc, curr) => {
-        if (!acc.includes(curr['Hora fim da aula']))
-            acc.push(curr['Hora fim da aula']);
-        return acc;
-    }, []).sort((a,b) => {
-        if(a != undefined && b != undefined){
-            const t1 = a.split(":");
-            const t2 = b.split(":");
-            if(t1[0]!= t2[0]){
-                return parseInt(t1[0])-parseInt(t2[0]);
-            } else {
-                return parseInt(t1[1])-parseInt(t2[1]);
-            } 
-        }
-        return 0;
-    }));
-}
-
-function findOpenSlots(DataIni,DataFim){
+function findOpenSlots(classroom,horario,DataIni,DataFim){
     var days = [];
     for(let date = new Date(DataIni.getTime()); date <= DataFim;date.setDate(date.getDate()+1)){
         days.push(new Date(date.getTime()));
     }
-    var salas = getSala();
+    fHorario = horario.filter(aula => {
+        if(aula['Data da aula'] != undefined){
+            const d = aula['Data da aula'].split("/");
+            const date = new Date(d[2], parseInt(d[1])-1, d[0]);
+            return date.getTime() >= DataIni.getTime() && date.getTime() <= DataFim.getTime();
+        }
+    });
+    var salas = listSalas(classroom);
     var daySala = [];
     var slots = [];
     days.forEach(data => {
         salas.forEach(sala =>{
-            daySala = getDisponibilidadeSala(sala,data);
+            daySala = getDisponibilidadeSala(fHorario,sala,data);
             daySala.forEach(time => {
                 slots.push({data:data,sala:sala,hora:time});
             });
@@ -112,26 +77,10 @@ function findOpenSlots(DataIni,DataFim){
     });
 
     return slots;
-    /*console.log(horario.filter( data => {
-        if(data['Data da aula'] != undefined){
-            const d = data['Data da aula'].split("/");
-            const date = new Date(d[2], parseInt(d[1])-1, d[0]);
-            return date>DataIni && date < DataFim;
-        }
-        return false;
-    }).sort((a,b) => {
-        if(a['Data da aula'] != undefined && b['Data da aula'] != undefined){
-            const d = a['Data da aula'].split("/");
-            const d1 = b['Data da aula'].split("/");
-            const date = new Date(d[2], parseInt(d[1])-1, d[0]);
-            const date1 = new Date(d1[2], parseInt(d1[1])-1, d1[0]);
-            return date-date1;
-        }
-        return 0;
-    }));*/
+    
 }
 
-function getSala(){
+function listSalas(classroom){
     let res = [];
     classroom.forEach(sala => {
         res.push(sala['Nome sala'])
@@ -140,12 +89,11 @@ function getSala(){
     return res;
 }
 
-function getDisponibilidadeSala(nome,dia){
+function getDisponibilidadeSala(horario,nome,dia){
     let daysOcupados = horario.filter((aula => {
         if(aula['Data da aula'] != undefined){
             const d = aula['Data da aula'].split("/");
             const date = new Date(d[2], parseInt(d[1])-1, d[0]);
-            //console.log(aula['Sala atribuída à aula']);
             return aula['Sala atribuída à aula'].trim() ===nome.trim()&& date.getTime() === dia.getTime();
         }
         return false;
